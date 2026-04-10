9365cb380919bd60f2f2580c0f73494fcbf579d1
chloemarguerite2008@gmail.com <chloemarguerite2008@gmail.com>
2026-04-10 23:41:30 +0000

    Created ServerModule1; Edited settings, Form1.ItemTemplate1 and Form1

---
+++ server_code/ServerModule1.py
@@ -0,0 +1,9 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+@anvil.server.callable
+def add_feedback(stage, text):
+  app_tables
+
