2b4bdbd2db07a57447a28e67da6752ef0c2aca15
chloemarguerite2008@gmail.com <chloemarguerite2008@gmail.com>
2026-04-11 02:20:10 +0000

    Created Form1.parchment_page.RowTemplate1 and Form1.parchment_page; Edited settings, ServerModule1 and Form1; Deleted Form1.ItemTemplate1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,8 +1,5 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
-import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -10,7 +7,33 @@ import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
-    anvil.users.login_with_form()
     self.init_components(**properties)
-
     # Any code you write here will run before the form opens.
+
+  @handle("home", "click")
+  def home_click(self, **event_args):
+    open_form("Form1")
+    pass
+
+  @handle("parchment", "click")
+  def home_click(self, **event_args):
+    open_form("Form1.parchment_page")
+    pass
+
+
+  stages = ["prewriting", "drafting", "revising"]
+    
+  @handle("prewriting", "click")
+  def prewriting_click(self, **event_args):
+      stage = [0]
+      pass
+  
+  @handle("drafting", "click")
+  def drafting_click(self, **event_args):
+    stage = [1]
+    pass
+
+  @handle("revising", "click")
+  def revising_click(self, **event_args):
+    stage = [2]
+    pass
--- client_code/Form1/parchment_page/__init__.py
+++ client_code/Form1/parchment_page/__init__.py
@@ -1,15 +1,12 @@
-from ._anvil_designer import ItemTemplate1Template
+from ._anvil_designer import parchment_pageTemplate
 from anvil import *
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
-import anvil.users
+import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.server
 
 
-class ItemTemplate1(ItemTemplate1Template):
+class parchment_page(parchment_pageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
