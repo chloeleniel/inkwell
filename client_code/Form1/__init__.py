69a098e2ab87f1a49a096d148adbc5c83e2b25de
chloemarguerite2008@gmail.com <chloemarguerite2008@gmail.com>
2026-04-11 00:13:58 +0000

    Edited settings, ServerModule1, Form1.ItemTemplate1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -7,7 +10,7 @@ import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
+    anvil.users.login_with_form()
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
