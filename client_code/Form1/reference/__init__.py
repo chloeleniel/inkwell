5662e35c2cb107a290dbfdd8e9561c488f78399e
chloemarguerite2008@gmail.com <chloemarguerite2008@gmail.com>
2026-05-06 05:49:45 +0000

    Edited Start, Form1.reference and Form1.parchment_page

--- client_code/Form1/reference/__init__.py
+++ client_code/Form1/reference/__init__.py
@@ -10,25 +10,25 @@ class reference(referenceTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.user_references.items = anvil.server.call('show_ref')
 
-    @handle("home", "click")
-    def home_click(self, **event_args):
-      open_form("Form1")
+  @handle("home", "click")
+  def home_click(self, **event_args):
+    open_form("Form1")
+  pass
+
+  @handle("parchment", "click")
+  def parchment_click(self, **event_args):
+    open_form("Form1.parchment_page")
     pass
 
-    @handle("parchment", "click")
-    def parchment_click(self, **event_args):
-      open_form("Form1.parchment_page")
-      pass
-  
-    @handle("feedback", "click")
-    def feedback_click(self, **event_args):
-      open_form("Form1.feedback_form")
-      pass
-  
-    @handle("ai_chat", "click")
-    def ai_chat_click(self, **event_args):
-      open_form("Form1.ai_chat")
-      pass
+  @handle("feedback", "click")
+  def feedback_click(self, **event_args):
+    open_form("Form1.feedback_form")
+    pass
 
-    self.user_references.items = anvil.server.call('show_ref')
+  @handle("ai_chat", "click")
+  def ai_chat_click(self, **event_args):
+    open_form("Form1.ai_chat")
+    pass
+
